72c9245715ba50185ae14241ffd863eee52a03f4
Alex Kemboi <akemboi@KWFTBANK.INT>
2023-04-07 23:44:55 +0300

    added select to the nav item

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-$('#myNav .dropdown-menu').dropdown();
+function toggleDropdown() {
+    var dropdown = document.getElementById("SoftwareDropdownMenu");
+    if (dropdown.style.display === "block") {
+      dropdown.style.display = "none";
+    } else {
+      dropdown.style.display = "block";
+    }
+  }
+  
+  var link = document.querySelector(".nav-item .nav-link");
+  link.addEventListener("click", toggleDropdown);
+  
