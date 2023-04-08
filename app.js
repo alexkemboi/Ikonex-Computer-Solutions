38104e42bd4f78f2f2715229875216b35b877248
Alex Kemboi <akemboi@KWFTBANK.INT>
2023-04-08 06:15:50 +0300

    changed the appearancce of the website

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-function toggleDropdown() {
-    var dropdown = document.getElementById("SoftwareDropdownMenu");
-    if (dropdown.style.display === "block") {
-      dropdown.style.display = "none";
-    } else {
-      dropdown.style.display = "block";
-    }
+const dropdownMenu = document.getElementById("dropdown-menu");
+const navItem = document.querySelector(".softwaresNavItem");
+
+navItem.addEventListener("mouseover", function() {
+  dropdownMenu.style.display = "block";
+});
+
+navItem.addEventListener("mouseout", function(event) {
+  // Check if the mouse has moved outside of both the nav item and the dropdown menu
+  if (!navItem.contains(event.relatedTarget) && !dropdownMenu.contains(event.relatedTarget)) {
+    dropdownMenu.style.display = "none";
   }
-  
-  var link = document.querySelector(".nav-item .nav-link");
-  link.addEventListener("click", toggleDropdown);
-  
+});
+
+dropdownMenu.addEventListener("mouseover", function() {
+  dropdownMenu.style.display = "block";
+});
+
+dropdownMenu.addEventListener("mouseout", function(event) {
+  // Check if the mouse has moved outside of both the nav item and the dropdown menu
+  if (!navItem.contains(event.relatedTarget) && !dropdownMenu.contains(event.relatedTarget)) {
+    dropdownMenu.style.display = "none";
+  }
+});
